60a44c9475a89d46774566a23a7276ed35ed7376
Wingerted <wingerted@gmail.com>
2013-09-06 13:34:00 +0800

    First commit

---
+++ semant.h
@@ -0,0 +1,24 @@
+#ifndef SEMANT_H_INCLUDED
+#define SEMANT_H_INCLUDED
+typedef void *Tr_exp;
+
+struct expty {
+  Tr_exp exp;
+  Ty_ty ty;
+};
+
+struct expty expTy (Tr_exp exp, Ty_ty ty) {
+  struct expty e;
+  e.exp = exp;
+  e.ty = ty;
+  return e;
+}
+
+
+struct expty transVar (S_table venv, S_table tenv, A_var v);
+struct expty transExp (S_table venv, S_table tenv, A_exp a);
+void transDec (S_table venv, S_table tenv, A_dec d);
+Ty_ty transTy (S_table tenv, A_ty a);
+
+
+#endif // SEMANT_H_INCLUDED
---
+++ tiger.tab.h
@@ -0,0 +1,125 @@
+/* A Bison parser, made by GNU Bison 2.5.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+/* Put the tokens into the symbol table, so that GDB and other debuggers
+   know about them.  */
+enum yytokentype {
+  ID = 258,
+  STRING = 259,
+  INT = 260,
+  COMMA = 261,
+  COLON = 262,
+  SEMICOLON = 263,
+  LPAREN = 264,
+  RPAREN = 265,
+  LBRACK = 266,
+  RBRACK = 267,
+  LBRACE = 268,
+  RBRACE = 269,
+  DOT = 270,
+  PLUS = 271,
+  MINUS = 272,
+  TIMES = 273,
+  DIVIDE = 274,
+  EQ = 275,
+  NEQ = 276,
+  LT = 277,
+  LE = 278,
+  GT = 279,
+  GE = 280,
+  AND = 281,
+  OR = 282,
+  ASSIGN = 283,
+  ARRAY = 284,
+  IF = 285,
+  THEN = 286,
+  ELSE = 287,
+  WHILE = 288,
+  FOR = 289,
+  TO = 290,
+  DO = 291,
+  LET = 292,
+  IN = 293,
+  END = 294,
+  OF = 295,
+  BREAK = 296,
+  NIL = 297,
+  FUNCTION = 298,
+  VAR = 299,
+  TYPE = 300,
+  UMINUS = 301
+};
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE {
+
+  /* Line 2068 of yacc.c  */
+#line 17 "tiger.y"
+
+  int pos;
+  int ival;
+  string sval;
+  A_var var;
+  A_exp exp;
+  A_dec dec;
+  A_ty ty;
+  A_decList decList;
+  A_expList expList;
+  A_field field;
+  A_fieldList fieldList;
+  A_fundec fundec;
+  A_fundecList fundecList;
+  A_namety namety;
+  A_nametyList nametyList;
+  A_efield efield;
+  A_efieldList efieldList;
+
+
+
+  /* Line 2068 of yacc.c  */
+#line 118 "tiger.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
